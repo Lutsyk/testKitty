1c0e4830ae21ac94767ba485cd934f58872303e1
Max Lutsenko <makc199710@me.com>
2022-02-01 14:43:20 +0200

    Updates

---
+++ precache-manifest.55c0b0ba6ca6c4eb736e1be40a168049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238c69ed43f2e071d9a3581187c2dbe6",
+    "url": "/testkitty/index.html"
+  },
+  {
+    "revision": "39dc1fd76eca0050f620",
+    "url": "/testkitty/static/css/main.d970adc6.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/testkitty/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "77a17ceacf3be5f46709",
+    "url": "/testkitty/static/js/3.061514ed.chunk.js"
+  },
+  {
+    "revision": "39dc1fd76eca0050f620",
+    "url": "/testkitty/static/js/main.5c4c1f2a.chunk.js"
+  },
+  {
+    "revision": "ba185fccb1e844e3f1ed",
+    "url": "/testkitty/static/js/runtime~main.e8e75767.js"
+  }
+]);
